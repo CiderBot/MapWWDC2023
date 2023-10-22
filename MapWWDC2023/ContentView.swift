9c8d3b0631a0aea90222f14ae3fe9141c62cb60d
Steven Yung <steve@redricebowl.com>
2023-10-22 14:12:00 -0700

    Initial Commit

---
+++ MapWWDC2023/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapWWDC2023
+//
+//  Created by Steven Yung on 10/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapWWDC2023/MapWWDC2023App.swift
@@ -0,0 +1,17 @@
+//
+//  MapWWDC2023App.swift
+//  MapWWDC2023
+//
+//  Created by Steven Yung on 10/22/23.
+//
+
+import SwiftUI
+
+@main
+struct MapWWDC2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
